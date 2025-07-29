f44e62b45d72f4a571577c6e88f25f048b6dfcdd
PABLO BETANCOURT <pablo.betancourt@usm.cl>
2025-07-28 23:37:21 -0400

    Movimiento finalizado

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,53 @@
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    int speed = 4;
+    [SerializeField]
+    int jump = 10;
+    [SerializeField]
+    float coyoteTime = 0.3f;
+    [SerializeField]
+    LayerMask groundLayer;
+    [SerializeField]
+    GameObject groundCheckCircle;
+
+    bool grounded = true;
+    float coyoteTimer=1;
+    float movementX;
+
+    Rigidbody2D rigidBody2D;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        rigidBody2D = this.GetComponent<Rigidbody2D>();
+        movementX = 0;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        grounded = Physics2D.OverlapCircle(groundCheckCircle.transform.position, 0.1f, groundLayer);
+
+        movementX = Input.GetAxis("Horizontal");
+        rigidBody2D.linearVelocity= new Vector2 (speed* movementX, rigidBody2D.linearVelocity.y);
+
+
+        if (grounded)
+        {
+            coyoteTimer = coyoteTime;
+        }
+        else
+        {
+            coyoteTimer -= Time.deltaTime;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && coyoteTimer>0)
+        {
+            rigidBody2D.linearVelocity = new Vector2(rigidBody2D.linearVelocity.x, this.jump);
+        }
+        print(coyoteTimer);
+    }
+    
+}
